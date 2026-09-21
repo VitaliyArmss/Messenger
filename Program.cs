986c9c05c6f7c7e93cae932f158f05f4e1547bc7
armos <armosother@gmail.com>
2026-09-21 23:39:01 +0700

    migrations

--- Program.cs
+++ Program.cs
@@ -119,6 +119,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.MapHub<ChatHub>("/chatHub");
 app.MapHub<NotificationHub>("/notificationHub");
 
